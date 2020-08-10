3d73c8e26c52bb8781963f9e75e6cb3ac56b1ea8
johnsjohns <o-ana@hotmail.com>
2020-08-09 22:28:52 -0300

    Terminado logica para verificação do formato de entrada

--- cep.js
+++ cep.js
@@ -1,3 +1,5 @@
+var espera;
+var textoEspera = "";
 function buscaCep(cep){
     var request = new XMLHttpRequest();
     request.onreadystatechange =  function() {
@@ -13,16 +15,62 @@ function buscaCep(cep){
 }
 
 function mostrar(arr){
-    console.log(arr);
-    document.getElementById("rua").value = arr.logradouro;
-    document.getElementById("bairro").value = arr.bairro;
-    document.getElementById("cidade").value = arr.localidade;
-    document.getElementById("estado").value = arr.uf;
+    clearInterval(espera);
+    if(arr.erro != true){
+        habilitar();
+        document.getElementById("rua").value = arr.logradouro;
+        document.getElementById("bairro").value = arr.bairro;
+        document.getElementById("cidade").value = arr.localidade;
+        document.getElementById("estado").value = arr.uf;
+    } else {
+        limpar();
+    }
+
 }
 
 function verifica(){
     var lido = document.getElementById("cep").value;
-    lido = lido.replace(/[^\d]+/g,'');
-    var myCep = "http://viacep.com.br/ws/" + lido + "/json"
-    buscaCep(myCep);
+    var novoReg = new RegExp(/[\d]{5}-[\d]{3}/);
+    if(novoReg.test(lido)){
+        lido = lido.replace(/[^\d]+/g,'');
+        desabilitar
+        limpar();
+        espera = setInterval(esperar, 20);
+        var myCep = "http://viacep.com.br/ws/" + lido + "/json"
+        buscaCep(myCep);
+    } else {
+        limpar();
+    }
+}
+
+function esperar(){
+    textoEspera += ".";
+    document.getElementById("rua").value = textoEspera;
+    document.getElementById("bairro").value = textoEspera;
+    document.getElementById("cidade").value = textoEspera;
+    document.getElementById("estado").value = textoEspera;
+    if(textoEspera.length > 30){
+        textoEspera = "";
+    }
+}
+
+function limpar(){
+    document.getElementById("rua").value = "";
+    document.getElementById("bairro").value = "";
+    document.getElementById("cidade").value = "";
+    document.getElementById("estado").value = "";
+}
+
+function desabilitar(){
+    document.getElementById("rua").disabled = true;
+    document.getElementById("bairro").disabled = true;
+    document.getElementById("cidade").disabled = true;
+    document.getElementById("estado").disabled = true;
+}
+
+function habilitar(){
+    document.getElementById("rua").disabled = false;
+    document.getElementById("bairro").disabled = false;
+    document.getElementById("cidade").disabled = false;
+    document.getElementById("estado").disabled = false;
 }
